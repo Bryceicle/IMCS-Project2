0ed80a56192fe3105acaf723721a4fc2ca31b855
Bryceicle <64384892+Bryceicle@users.noreply.github.com>
2025-01-18 15:02:05 -0500

    LSB training dataset update 1 - Jan18
    added and refined load_image_dataset() function to load in the LSB training dataset.

--- imcs-project-2.py
+++ imcs-project-2.py
@@ -6,18 +6,24 @@ Bryce Gill - 100666638
 
 """
 
-from tqdm import tqdm
+import cv2
+from docopt import docopt
 import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as data
-import torchvision
-import torchvision.transforms as transforms
+import os
 
-
-
-class LCB_CN(nn.module):
-    def __init__(self, in_channels, num_classes):
-        super(Net, self).__init__()
+def load_image_dataset(path):
+    dir_list = os.listdir(path)
+    image_list = []
     
+    for file in dir_list:
+        image = cv2.imread(path+'//'+file)
+        image_list.append(image)
+        
+    return image_list
+
+train_clean_path = 'C://imcsProject2//Stego-Images-Dataset-(LSB)//train//train//clean'
+train_stego_path = 'C://imcsProject2//Stego-Images-Dataset-(LSB)//train//train//stego'
+
+train_clean_dataset = load_image_dataset(train_clean_path)
+train_stego_dataset = load_image_dataset(train_stego_path)
+
