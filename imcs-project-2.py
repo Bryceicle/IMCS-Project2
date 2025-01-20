2b03f70c9743be7035c3c5761e660a3faffdb2b4
Bryceicle <64384892+Bryceicle@users.noreply.github.com>
2025-01-19 22:06:35 -0500

    added load_lsb_train_arrays()
    created load_lsb_train_arrays() to load in the lsb training sets for nn training

--- imcs-project-2.py
+++ imcs-project-2.py
@@ -7,11 +7,17 @@ Bryce Gill - 100666638
 """
 
 import cv2
-from docopt import docopt
 import numpy as np
+from numpy import asarray
+from numpy import savetxt
+from numpy import loadtxt
 import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
 
-def load_image_dataset(path):
+def load_image_dataset_to_array(path):
     dir_list = os.listdir(path)
     image_list = []
     
@@ -21,9 +27,51 @@ def load_image_dataset(path):
         
     return image_list
 
-train_clean_path = 'C://imcsProject2//Stego-Images-Dataset-(LSB)//train//train//clean'
-train_stego_path = 'C://imcsProject2//Stego-Images-Dataset-(LSB)//train//train//stego'
+def load_lsb_train_array_from_file():
+    
+    if os.path.exists('C:\imcs3010\IMCS-Project2\clean_lsb_train_data.npy'):
+        clean = loadtxt('clean_lsb_train_data.npy', delimiter=',')
+        print('found')
+    else: 
+        print('not found')
+        return 0
+    if os.path.exists('C:\imcs3010\IMCS-Project2\stego_lsb_train_data.npy'):
+        stego = loadtxt('stego_lsb_train_data.npy', delimiter=',')
+    else: 
+        return 0
+    
+    return clean, stego
 
-train_clean_dataset = load_image_dataset(train_clean_path)
-train_stego_dataset = load_image_dataset(train_stego_path)
+def load_lsb_train_arrays(path):
+    
+    if load_lsb_train_array_from_file() == 0:
+        train_clean_path = path+'//train//train//clean'
+        train_stego_path = path+'//train//train//stego'
+        train_clean_dataset = load_image_dataset_to_array(train_clean_path)
+        train_stego_dataset = load_image_dataset_to_array(train_stego_path)
+        clean_dataset = asarray(train_clean_dataset)
+        stego_dataset = asarray(train_stego_dataset)
+        savetxt('clean_lsb_train_data.npy', clean_dataset, delimiter=',')
+        savetxt('stego_lsb_train_data.npy', stego_dataset, delimiter=',')
+        print('saved!')
+    else:
+        train_clean_dataset, train_stego_dataset = load_lsb_train_array_from_file()
+        print('loaded!')
+        
+    return train_clean_dataset, train_stego_dataset
+    
 
+class Net(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(Net, self).__init__()
+        
+lsb_dataset_path = 'C://imcs3010//LSB Dataset'
+        
+def main():
+    
+    train_clean_dataset, train_stego_dataset = load_lsb_train_arrays(lsb_dataset_path)
+    
+    return
+
+main()
+    
